481961b3c0e1cae8838a54a7f0ce81e70a9496e8
Ambar Septian <ambar.septian@tokopedia.com>
2020-02-19 21:53:06 +0700

    Initial Commit

---
+++ SnapshotSharingSession/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapshotSharingSession
+//
+//  Created by Ambar Septian on 19/02/20.
+//  Copyright © 2020 Ambar Septian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SnapshotSharingSessionTests/SnapshotSharingSessionTests.swift
@@ -0,0 +1,34 @@
+//
+//  SnapshotSharingSessionTests.swift
+//  SnapshotSharingSessionTests
+//
+//  Created by Ambar Septian on 19/02/20.
+//  Copyright © 2020 Ambar Septian. All rights reserved.
+//
+
+import XCTest
+@testable import SnapshotSharingSession
+
+class SnapshotSharingSessionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
